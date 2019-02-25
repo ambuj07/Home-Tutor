4e2358513135536f61dd4590ecf2a7b2677c9fbf
ambuj@truweight.in <ambuj@truweight.in>
2019-02-26 01:00:22 +0530

    student registration changes

--- src/app/student-registration/student-registration.component.ts
+++ src/app/student-registration/student-registration.component.ts
@@ -23,26 +23,58 @@ export class StudentRegistrationComponent implements OnInit {
   
       // next step
       $('.registration-form .btn-next').on('click', function () {
-          var parent_fieldset = $(this).parents('.fieldset');
-          var next_step = true;
-  
-          parent_fieldset.find('select').each(function () {
-              console.log($(this).is(":visible") +" sds");
-              if ($(this).is(":visible") && ($(this).val() == null || $(this).val() == "")) {
-                  $(this).addClass('input-error');
-                  next_step = false;
-              } else {
-                  $(this).removeClass('input-error');
-              }
-          });
-  
-          if (next_step) {
-              parent_fieldset.fadeOut(400, function () {
-                  $(this).next().fadeIn();
-              });
+        var parent_fieldset = $(this).parents('.fieldset');
+        var next_step = true;
+        var mobileRegex = /^[1-9]\d{9}$/;
+        var mobile = $("#mobileNumber").val();
+        var whatsapp = $("#whatsappNumber").val();
+        var emailRegex = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+        var allFilled = true;
+
+        $(this).parents('.fieldset').find('select,input').each(function () {
+          if ($(this).is(":visible") && ($(this).val() == null || $(this).val() == "")) {
+              $(this).addClass('input-error');
+              $(this).closest(".bootstrap-select").find(".dropdown-toggle").css("border-color","#d03e3e");
+              next_step = false;
+              allFilled = false;
+              showToast("All fields are Mandatory");
+          } else {
+              $(this).removeClass('input-error');
+              $(this).closest(".bootstrap-select").find(".dropdown-toggle").css("border-color","#ccc"); 
           }
-  
-      });
+        });
+        if(allFilled){
+          if($("#email").is(":visible") && !emailRegex.test($("#email").val())){
+            next_step = false;
+            $("#email").addClass('input-error');
+            showToast("Please enter a valid email");
+            return false;
+          }else{
+            $("#email").removeClass('input-error');
+          }
+          if($("#mobileNumber").is(":visible") && !mobileRegex.test(mobile)){
+            next_step = false;
+            $("#mobileNumber").addClass('input-error');
+            showToast("Invalid Mobile Number : Should be only 10 digit");
+            return false;
+          }else{
+            $("#mobileNumber").removeClass('input-error');
+          }
+          if($("#whatsappNumber").is(":visible") && !mobileRegex.test(whatsapp)){
+            next_step = false;
+            $("#whatsappNumber").addClass('input-error');
+            showToast("Invalid WhatsApp Number : Should be only 10 digit");
+          }else{
+            $("#whatsappNumber").removeClass('input-error');
+          } 
+        }
+        if (next_step) {
+            parent_fieldset.fadeOut(400, function () {
+                $(this).next().fadeIn();
+            });
+        }
+
+    });
   
       // previous step
       $('.registration-form .btn-previous').on('click', function () {
@@ -50,8 +82,6 @@ export class StudentRegistrationComponent implements OnInit {
               $(this).prev().fadeIn();
           });
       });
-  
-      // submit
 
       //show on change
       const prepAndKg = ['Prep','Nursurry','LKG','UKG']
@@ -106,7 +136,7 @@ export class StudentRegistrationComponent implements OnInit {
             for(var i=0; i<passion.length; i++)
             classHtml += '<option value="'+passion[i]+'">'+passion[i]+'</option>';
         }
-        $("#chooseClass").html(classHtml);
+        $("#chooseClass").html(classHtml).selectpicker('refresh');
         $("#chooseClassDiv").prop('hidden',false);
         $("#chooseSubjectDiv").prop('hidden',true);
       });
@@ -176,123 +206,223 @@ export class StudentRegistrationComponent implements OnInit {
             subjectHtml += '<option value="'+musicSubject[i]+'">'+musicSubject[i]+'</option>';
         }
 
-        $("#chooseSubject").html(subjectHtml);
+        $("#chooseSubject").html(subjectHtml).selectpicker('refresh');
         $("#chooseSubjectDiv").prop('hidden',false);
       });
 
+      //city and state
+      var statesHtml = '<option value="" selected disabled>Select Your State</option>';
+    
+      $.ajax({
+        type: 'GET',
+        url: baseUrl+"/config/state",
+        async:false,
+        contentType: "application/json;charset=utf-8",
+        success: function(resultData) { 
+            console.log(resultData);
+            resultData.forEach(function (a){
+              statesHtml += '<option value="'+a.id+'">'+a.name+'</option>';
+            })
+          }
+      });
+      $("#states").html(statesHtml).selectpicker();
+      
+      $('#states').change(function(){
+        var cityHtml = '<option value="" selected disabled>Select Your City</option>';
+        $.ajax({
+          type: 'GET',
+          url: baseUrl+"/config/"+$(this).val()+"/city",
+          async:false,
+          contentType: "application/json;charset=utf-8",
+          success: function(resultData) { 
+            resultData.forEach(function (a){
+                cityHtml += '<option value="'+a.name+'">'+a.name+'</option>';
+              });
+            }
+          });
+          $("#city").html(cityHtml).selectpicker();
+          $("#city").selectpicker('refresh');
+          $("#cityDiv").prop('hidden',false);
+        });
+
      
 
-      $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCTXSw8ekU_Kua38p6IvBgTU1_74sJYiEY&libraries=places", function(){
+    //   $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCTXSw8ekU_Kua38p6IvBgTU1_74sJYiEY&libraries=places", function(){
   
-    //   var placeSearch, autocomplete;
-    //   var componentForm = {
-    //     address: 'address'
-    //   };
+    // //   var placeSearch, autocomplete;
+    // //   var componentForm = {
+    // //     address: 'address'
+    // //   };
       
-    //   function initAutocomplete() {
-    //     autocomplete = new google.maps.places.Autocomplete(
-    //       (document.getElementById('address')), {
-    //         zoom: 13
+    // //   function initAutocomplete() {
+    // //     autocomplete = new google.maps.places.Autocomplete(
+    // //       (document.getElementById('address')), {
+    // //         zoom: 13
+    // //       });
+    // //     fillInAddress();
+    // //   }
+      
+    // //   function fillInAddress() {
+    // //     var place = autocomplete.getPlace();
+    // //     console.log(autocomplete.getPlace()+" awddw");
+      
+    // //     for (var component in componentForm) {
+    // //         (<HTMLInputElement>document.getElementById(component)).value = '';
+    // //     }
+      
+    // //    for (var i = 0; i < place.address_components.length; i++) {
+    // //       var addressType = place.address_components[i].types[0];
+    // //       console.log(place.address_components[i]+"sdcsd cds");
+    // //       if (componentForm[addressType]) {
+    // //         var val = place.address_components[i][componentForm[addressType]];
+    // //         (<HTMLInputElement>document.getElementById(addressType)).value = val;
+    // //       }
+    // //     }
+    // //   }
+    // bindAutocomplete();
+    // function bindAutocomplete() {
+    
+    //     var defaultBounds = new google.maps.LatLngBounds(new google.maps.LatLng(20.5937, 78.9629), new google.maps.LatLng(20.5937, 78.9629));
+    
+    //     var options = {
+    //         bounds: defaultBounds,
+    //         zoom: 13,
+    //         types: ['geocode']
+    //     };
+    
+    //     var autocomplete = new google.maps.places.Autocomplete(document.getElementById('address'), options);
+    
+    //     google.maps.event.addListener(autocomplete, 'place_changed', function () {
+    //         var place = autocomplete.getPlace();
+    //         console.log(place);
+    //     });
+    // }
+      
+    //   function geolocate() {
+    //       if (navigator.geolocation) {
+    //       navigator.geolocation.getCurrentPosition(function(position) {
+    //         var geolocation = {
+    //           lat: position.coords.latitude,
+    //           lng: position.coords.longitude
+    //         };
+    //           $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng="+geolocation.lat+","+geolocation.lng+"&sensor=true",function(data){
+    //             console.log(data)  
+    //             $("#address").val(data.results[0].formatted_address);
+    //               console.log(data.results[0].formatted_address+" dsdsdds");
+    //           });
     //       });
-    //     fillInAddress();
+    //     }	
     //   }
-      
-    //   function fillInAddress() {
-    //     var place = autocomplete.getPlace();
-    //     console.log(autocomplete.getPlace()+" awddw");
-      
-    //     for (var component in componentForm) {
-    //         (<HTMLInputElement>document.getElementById(component)).value = '';
-    //     }
-      
-    //    for (var i = 0; i < place.address_components.length; i++) {
-    //       var addressType = place.address_components[i].types[0];
-    //       console.log(place.address_components[i]+"sdcsd cds");
-    //       if (componentForm[addressType]) {
-    //         var val = place.address_components[i][componentForm[addressType]];
-    //         (<HTMLInputElement>document.getElementById(addressType)).value = val;
-    //       }
-    //     }
-    //   }
-    bindAutocomplete();
-    function bindAutocomplete() {
-    
-        var defaultBounds = new google.maps.LatLngBounds(new google.maps.LatLng(20.5937, 78.9629), new google.maps.LatLng(20.5937, 78.9629));
-    
-        var options = {
-            bounds: defaultBounds,
-            zoom: 13,
-            types: ['geocode']
-        };
-    
-        var autocomplete = new google.maps.places.Autocomplete(document.getElementById('address'), options);
-    
-        google.maps.event.addListener(autocomplete, 'place_changed', function () {
-            var place = autocomplete.getPlace();
-            console.log(place);
-        });
-    }
-      
-      function geolocate() {
-          if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var geolocation = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-              $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng="+geolocation.lat+","+geolocation.lng+"&sensor=true",function(data){
-                console.log(data)  
-                $("#address").val(data.results[0].formatted_address);
-                  console.log(data.results[0].formatted_address+" dsdsdds");
-              });
-          });
-        }	
-      }
-      $("#getLocation").on('click',function(){
-          $("#address").val("please wait getting your location");
-          geolocate();
-      })
+    //   $("#getLocation").on('click',function(){
+    //       $("#address").val("please wait getting your location");
+    //       geolocate();
+    //   })
       
 
-    //   $("#address").on('input',function(){
-    //     initAutocomplete();
-    //   })
+    // //   $("#address").on('input',function(){
+    // //     initAutocomplete();
+    // //   })
+    // });
+    var data = '';
     $("#submit").click(function(){
         var error = false;  
-        $(".registration-form").find('input[type="text"],input[type="email"]').each(function () {
-            if ($(this).val() == "") {
+        var error = false;  
+          $(this).parents('.fieldset').find('select,input').each(function () {
+            if ($(this).is(":visible") && ($(this).val() == null || $(this).val() == "")) {
                 $(this).addClass('input-error');
+                $(this).closest(".bootstrap-select").find(".dropdown-toggle").css("border-color","#d03e3e");
                 error = true;
+                showToast("All fields are Mandatory");
             } else {
                 $(this).removeClass('input-error');
+                $(this).closest(".bootstrap-select").find(".dropdown-toggle").css("border-color","#ccc");
             }
-        });
+          });
           if(!error){
-            var name = $("#name").val();
-            var location = $("#address").val(); 
-            var mobile = $("#mobileNumber").val(); 
-            var email = $("#email").val(); 
             var classcategory = $("#classGroup").val();  
             var particularClass = $("#chooseClass").val(); 
-            var subjects = $("#chooseSubject").val(); 
-            var data = '{"id":null,"name":"'+name+'","location":"'+location+'","mobile":"'+mobile+'","email":"'+email+'","category":"","classcategory":"'+classcategory+'","particularClass":"'+particularClass+'","subjects":"'+subjects+'"}';
-            $.ajax({
-                type: 'POST',
-                url: baseUrl+"/student",
-                contentType: "application/json;charset=utf-8",
-                data: data,
-                success: function(resultData) { 
-                    console.log(resultData);
-                    localStorage.setItem("userName",resultData.detail.name);
-                    localStorage.setItem("type",resultData.type);
-                    localStorage.setItem("userId",resultData.refId);
-                    localStorage.setItem("from_reg","Yes");
-                    window.location.href = '/dashboard/student/'+resultData.refId;
-                 }
-            });  
+            var subjects = $("#chooseSubject").val();
+            var addrLine1 = $("#addrLine1").val();
+            var location = $("#location").val()
+            var zipCode = $("#pinCode").val() 
+            var states = $("#states").val() 
+            var city = $("#city").val() 
+            var name = $("#name").val()  
+            var email = $("#email").val();  
+            var mobile = $("#mobileNumber").val(); 
+            var whatsappNumber = $("#whatsappNumber").val();
+            var turorType = $("#tutorType").val();
+            var preferGender = $("#preferGender").val();
+            var preferTiming = $("#preferTiming").val();
+            var preferDay = $("#preferDay").val();
+            var preferFee = $("#preferFee").val();
+            var studentPerformance = $("#studentPerformance").val();
+            var reasonForQuery = $("#reasonForQuery").val();
+            var anythingElse = $("#anythingElse").val();
+            data = '{"id":null,"category":"","classcategory":"'+classcategory+'","particularClass":"'+particularClass+'","subjects":"'+subjects+'","name":"'+name+'","addrLine1":"'+addrLine1+'","location":"'+location+'","zipCode":"'+zipCode+'","states":"'+states+'","city":"'+city+'","email":"'+email+'","mobile":"'+mobile+'","whatsappNumber":"'+whatsappNumber+'","turorType":"'+turorType+'","preferGender":"'+preferGender+'","preferTiming":"'+preferTiming+'","preferDay":"'+preferDay+'","preferFee":"'+preferFee+'","performance":"'+studentPerformance+'","reasonForQuery":"'+reasonForQuery+'","anythingElse":"'+anythingElse+'"}';
+            $("#myProfileModal").modal('show');
+            var html = '<table class="table table-bordered">';
+                html += '<tr><td><b>Class Category : </b>'+classcategory+'</td><td><b>Class: </b>'+particularClass+'</td></tr>';
+                html += '<tr><td colspan="2"><b>Subjects : </b>'+subjects+'</td></tr>';
+                html += '<tr><td colspan="2"><b>Address: </b>'+addrLine1+', '+location+', '+city+', '+states+', '+zipCode+'</td></tr>';
+                html += '<tr><td><b>Name : </b>'+name+'</td><td>Email : </b>'+email+'</td></tr>';
+                html += '<tr><td><b>Mobile : </b>'+mobile+'</td><td>Whatsapp : </b>'+whatsappNumber+'</td></tr>';
+                html += '<tr><td><b>Turor Type : </b>'+turorType+'</td><td><b>Prefer Gender : </b>'+preferGender+'</td></tr>';
+                html += '<tr><td><b>Prefer Timing : </b>'+preferTiming+'</td><td><b>Prefer Day : </b>'+preferDay+'</td></tr>';
+                html += '<tr><td><b>Prefer Fee : </b>'+preferFee+'</td><td><b>Performance : </b>'+studentPerformance+'</td></tr>';
+                html += '<tr><td colspan="2"><b>Reason For Query : </b>'+reasonForQuery+'</td></tr>';
+                html += '<tr><td colspan="2"><b>Anything Else : </b>'+anythingElse+'</td></tr>';
+                html += '</table>';
+                html += '<div style="text-align:center">';
+                html += '<button type="button" id="submitData" class="btn btn-next btn-register">POST YOUR REQUIREMENT NOW</button>';
+                html += '</div>';
+                $("#myProfileModal").find(".modal-body").html(html);
+
+            // $.ajax({
+            //     type: 'POST',
+            //     url: baseUrl+"/student",
+            //     contentType: "application/json;charset=utf-8",
+            //     data: data,
+            //     success: function(resultData) { 
+            //         console.log(resultData);
+            //         localStorage.setItem("userName",resultData.detail.name);
+            //         localStorage.setItem("type",resultData.type);
+            //         localStorage.setItem("userId",resultData.refId);
+            //         localStorage.setItem("from_reg","Yes");
+            //         window.location.href = '/dashboard/student/'+resultData.refId;
+            //      }
+            // });  
           }
     });
+
+    $(document).on('click','#submitData',function(){
+        $.ajax({
+            type: 'POST',
+            //url: baseUrl+"/student",
+            contentType: "application/json;charset=utf-8",
+            data: data,
+            // success: function(resultData) { 
+            //     console.log(resultData);
+            //     localStorage.setItem("userName",resultData.detail.name);
+            //     localStorage.setItem("type",resultData.type);
+            //     localStorage.setItem("userId",resultData.refId);
+            //     localStorage.setItem("from_reg","Yes");
+            //     window.location.href = '/dashboard/student/'+resultData.refId;
+            // }
+        }); 
     });
+
+    //general functions
+    function showToast(data) {
+        var x = document.getElementById("snackbar");
+        x.className = "show";
+        x.innerText = data;
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+      }
+      $(document).on('change','.selectpicker',function() {
+        $(this).next().blur();
+      });
+
   });
   }
 
